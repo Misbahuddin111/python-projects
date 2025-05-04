ee04ef3f59f0dd7a0e7c67c60b96c5b1873a92bf
Misbahuddin <misbahdin63@gmail.com>
2025-05-04 15:58:36 +0500

    guess number game

---
+++ 2 . guess nun.py
@@ -0,0 +1,19 @@
+import random
+number = random.randint(1,100)
+attempts = 0
+
+print("please enter number between 1 and 100")
+while True:
+    guess = int(input('please enter a number'))
+    attempts += 1
+
+    if guess < number:
+        print("too low")
+    elif guess > number:
+        print("too high")
+    else:
+        print(f"you correct guess number in {attempts} attempts")
+        break
+
+
+    
